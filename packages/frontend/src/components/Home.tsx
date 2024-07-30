0bbf41a94d81bfa3a2845ab72ccf951ff786f975
Oleh Khoroshykh <oleg.khoroshikh@arbitas.com>
2024-07-30 14:10:28 +0300

    Adding the badges of reservation on Calendar

--- packages/frontend/src/components/Home.tsx
+++ packages/frontend/src/components/Home.tsx
@@ -1,11 +1,5 @@
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DateCalendar } from "@mui/x-date-pickers";
+import Reservation from "./Reservation";
 
 export default function Home() {
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar />
-    </LocalizationProvider>
-  );
+  return <Reservation />;
 }
---
+++ packages/frontend/src/components/Reservation.tsx
@@ -0,0 +1,134 @@
+import { useEffect, useRef, useState } from "react";
+import { PickersDayProps } from "@mui/lab";
+import { Badge } from "@mui/material";
+import {
+  DateCalendar,
+  DayCalendarSkeleton,
+  LocalizationProvider,
+  PickersDay,
+} from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
+
+function getRandomNumber(min: number, max: number) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+/**
+ * Mimic fetch with abort controller https://developer.mozilla.org/en-US/docs/Web/API/AbortController/abort
+ * ⚠️ No IE11 support
+ */
+function fakeFetch(date: Dayjs, { signal }: { signal: AbortSignal }) {
+  return new Promise<{ daysToHighlight: number[] }>((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      const daysInMonth = date.daysInMonth();
+      const daysToHighlight = [1, 2, 3].map(() =>
+        getRandomNumber(1, daysInMonth)
+      );
+
+      resolve({ daysToHighlight });
+    }, 500);
+
+    signal.onabort = () => {
+      clearTimeout(timeout);
+      reject(new DOMException("aborted", "AbortError"));
+    };
+  });
+}
+
+const initialValue = dayjs(Date.now());
+
+function ServerDay(
+  props: PickersDayProps<Dayjs> & { highlightedDays?: number[] }
+) {
+  const {
+    highlightedDays = [],
+    day,
+    onDaySelect,
+    outsideCurrentMonth,
+    isFirstVisibleCell,
+    isLastVisibleCell,
+    ...other
+  } = props;
+  const isSelected = highlightedDays.indexOf(props.day.date()) >= 0;
+
+  return (
+    <Badge
+      key={props.day.toString()}
+      overlap="circular"
+      badgeContent={isSelected ? "🌚" : undefined}
+    >
+      <PickersDay
+        {...other}
+        day={day}
+        onDaySelect={onDaySelect}
+        outsideCurrentMonth={outsideCurrentMonth}
+        isFirstVisibleCell={isFirstVisibleCell}
+        isLastVisibleCell={isLastVisibleCell}
+      />
+    </Badge>
+  );
+}
+
+export default function Reservation() {
+  const requestAbortController = useRef<AbortController | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [highlightedDays, setHighlightedDays] = useState([1, 2, 15]);
+
+  const fetchHighlightedDays = (date: Dayjs) => {
+    const controller = new AbortController();
+    fakeFetch(date, {
+      signal: controller.signal,
+    })
+      .then(({ daysToHighlight }) => {
+        setHighlightedDays(daysToHighlight);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        // ignore the error if it's caused by `controller.abort`
+        if (error.name !== "AbortError") {
+          throw error;
+        }
+      });
+
+    requestAbortController.current = controller;
+  };
+
+  useEffect(() => {
+    fetchHighlightedDays(initialValue);
+    // abort request on unmount
+    return () => requestAbortController.current?.abort();
+  }, []);
+
+  const handleMonthChange = (date: Dayjs) => {
+    if (requestAbortController.current) {
+      // make sure that you are aborting useless requests
+      // because it is possible to switch between months pretty quickly
+      requestAbortController.current.abort();
+    }
+
+    setIsLoading(true);
+    setHighlightedDays([]);
+    fetchHighlightedDays(date);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar
+        defaultValue={initialValue}
+        loading={isLoading}
+        onMonthChange={handleMonthChange}
+        renderLoading={() => <DayCalendarSkeleton />}
+        slots={{
+          day: ServerDay,
+        }}
+        slotProps={{
+          day: {
+            highlightedDays,
+          } as any,
+        }}
+        showDaysOutsideCurrentMonth
+      />
+    </LocalizationProvider>
+  );
+}
